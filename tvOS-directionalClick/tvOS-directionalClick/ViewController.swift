eb615e2e316a8e62323536737b332c535b097229
Steve Mayhew <mayhew.steve@gmail.com>
2019-02-13 15:48:38 -0800

    Fixed to support 4 axis (up/down/left/right) and fix logic errors

--- tvOS-directionalClick/tvOS-directionalClick/ViewController.swift
+++ tvOS-directionalClick/tvOS-directionalClick/ViewController.swift
@@ -15,25 +15,41 @@ class ViewController: UIViewController {
     
     private var dPadState: DPadState = .select
 
+    private var padNeedsSetup: Bool = true
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        setUpDirectionalPad()
     }
     
     override func pressesBegan(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
         
+        if padNeedsSetup {
+            setUpDirectionalPad()
+            padNeedsSetup = false
+        }
+        print("pressesBegan: \(dPadState)")
+
         for press in presses {
             switch press.type {
             case .select where dPadState == .up:
                 directionLabel.text = "⬆️"
             case .select where dPadState == .down:
                 directionLabel.text = "⬇️"
+            case .select where dPadState == .left:
+                directionLabel.text = "⬅️"
+            case .select where dPadState == .right:
+                directionLabel.text = "➡️"
             case .select:
                 directionLabel.text = "🆗"
             default:
                 super.pressesBegan(presses, with: event)
             }
         }
+        directionLabel.setNeedsDisplay()
+    }
+    
+    override func pressesEnded(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
+        print("pressesEnded: \(dPadState)")
     }
     
     // MARK: - Private
@@ -45,17 +61,25 @@ class ViewController: UIViewController {
         micro.dpad.valueChangedHandler = {
             [weak self] (pad, x, y) in
             
-            let threshold: Float = 0.7
-            if y > threshold {
-                self?.dPadState = .up
+            if (abs(x) > 0.0 && abs(y) > 0.0) {
+                print("x/y \(x) \(y)")
+                let threshold: Float = 0.7
+                if abs(y) > threshold {
+                    if y > 0.0 {
+                        self?.dPadState = .up
+                    } else {
+                        self?.dPadState = .down
+                    }
+                } else if abs(x) > threshold {
+                    if x > 0.0 {
+                        self?.dPadState = .right
+                    } else {
+                        self?.dPadState = .left
+                    }
+                } else {
+                    self?.dPadState = .select
+                }
             }
-            else if y < -threshold {
-                self?.dPadState = .down
-            }
-            else {
-                self?.dPadState = .select
-            }
-            
         }
     }
 }
